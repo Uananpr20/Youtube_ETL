cb67fb7a4b3e9411bdd08c99976807f1e40b0612
Uananpr20 <p-ananth@hotmail.com>
2026-02-27 19:18:53 -0500

    setup the DAG execution script

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -4,18 +4,19 @@ from datetime import date
 
 import os
 from dotenv import load_dotenv
+from airflow.decorators import task
+from airflow.models import Variable
 
 
 load_dotenv(dotenv_path = "./.env")
 
 
 
-API_KEY = os.getenv("API_KEY")
-
-
-channel_handle = "MrBeast"
+API_KEY = Variable.get("API_KEY")
+channel_handle = Variable.get("CHANNEL_HANDLE")
 maxResults = 50 
 
+@task
 def get_playlist_id():
 
     try:
@@ -37,7 +38,7 @@ def get_playlist_id():
 
 
 
-
+@task
 def get_video_ids(playlist_id):
 
     video_ids = []
@@ -75,7 +76,7 @@ def get_video_ids(playlist_id):
 
 
 
-
+@task
 def extract_video_data(video_ids):
      extracted_data = []
 
@@ -121,7 +122,7 @@ def extract_video_data(video_ids):
          raise e
      
 
-
+@task
 def save_to_json(extract_data):
     file_path = f"./data/YT_ETL_data_{date.today()}.json"
 
